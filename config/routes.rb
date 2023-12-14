4153a4eac770c212f47b517ba45a1a6d35d6ccd0
Agustin Soria <soriaagustin8@gmail.com>
2023-12-13 22:44:59 -0300

    Adding route new config

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    get '/greetings', to: 'greetings#index'
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get '*page', to: "static#index", constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "static#index"
 end
